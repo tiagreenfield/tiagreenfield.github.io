8e99fd4b7c4fd58108d33feb09e3cd43b37d6617
tiagreenfield <tg1274@bard.edu>
2016-08-17 10:05:57 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -5,7 +5,7 @@
  */
 
 var things = ["Rose", "Tia", "Nico", "Ben", "Sullivan", "Samira"];
-var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect"]
+var programmers = ["roses", "Tia", "choreographer", "director", "mathematician", "architect"]
 var castles = ["museums", "bridges", "cities"]
 var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
 var airs = ["sky", "atmosphere", "heavens", "ether", "wind"];
